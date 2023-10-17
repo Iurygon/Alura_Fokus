48d9eb3b3957d73dbdad66b2acd2a7b62c13b1c6
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-10-17 19:12:16 -0300

    Continuidade no curso

--- main.js
+++ main.js
@@ -2,25 +2,63 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const buttons = document.querySelectorAll('.app__card-button');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+
+function mudaContexto(contexto){
+
+    buttons.forEach ((contexto) => {
+        
+        contexto.classList.remove('active');
+
+    });
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `imagens/${contexto}.png`)
+    switch(contexto){
+
+        case 'foco':
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+
+    };
+
+};
 
 focoBt.addEventListener('click', () => {
 
-    html.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', 'imagens/foco.png');
+    mudaContexto('foco');
+    focoBt.classList.add('active');
 
 });
 
 curtoBt.addEventListener('click', () => {
 
-    html.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', 'imagens/descanso-curto.png');
+   mudaContexto('descanso-curto');
+   curtoBt.classList.add('active');
 
 });
 
 longoBt.addEventListener('click', () => {
     
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', 'imagens/descanso-longo.png');
+    mudaContexto('descanso-longo');
+    longoBt.classList.add('active');
 
 });
